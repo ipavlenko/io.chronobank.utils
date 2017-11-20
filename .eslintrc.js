6a6d24fd0ffd9ce0d0dec3b222023dd9db0aa3d3
Igor Pavlenko <ipavlenko@inbox.ru>
2017-11-20 18:45:00 +0500

    Implemented

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "root": true,
+  "extends": "standard",
+  env: {
+    node: true
+  },
+  "globals": {
+    requireRoot: true
+  },
+  "plugins": [
+  ],
+  "rules": {
+  }
+}
